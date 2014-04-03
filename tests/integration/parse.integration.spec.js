cb5d34d7f686974ff65831e77fe79a29f891abc1
Jonathan El-Bizri <git@elbizri.com>
2014-04-03 16:42:56 -0700

    parser retreives Urls and matches dates

--- tests/integration/parse.integration.spec.js
+++ tests/integration/parse.integration.spec.js
@@ -1,7 +1,6 @@
 var _ = require('lodash');
 var sinon = require('sinon');
 require('jasmine-sinon');
-var proxyquire = require('proxyquire');
 
 var parser = require('../../modules/parser');
 
@@ -9,7 +8,14 @@ describe("Parser|", function() {
   it("should exist", function() {
     expect(parser).toBeDefined();
   });
-	it("should return an array", function() {
-	  expect(Array.isArray(parser("<html></html>"))).toBeTruthy();
-	});
+  describe(".fromXml", function() {
+		it("should return an array", function() {
+		  expect(Array.isArray(parser.fromXml("<html></html>"))).toBeTruthy();
+		});
+		it("should return an array", function() {
+			var result = parser.fromXml('<html><body><a href="test">TestUrl</a></body></html>')
+		  expect(Array.isArray(result)).toBeTruthy();
+		});    
+  });
+
 });
